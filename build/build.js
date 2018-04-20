74d2ca9eb2fc86aaab5d5cd107e8e7b39e6db969
Tony Yip <tony@opensource.hk>
2018-04-21 04:27:21 +0800

    Finish build script

---
+++ build/build.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const shell = require('shelljs');
+const fs = require('fs');
+const ejs = require('ejs');
+const rendererConfig = require('../config/webpack.prod.conf');
+const mainConfig = require('../config/webpack.main.conf');
+
+shell.rm('-rf', 'dist');
+
+shell.cp('-r', 'build/assets', 'dist');
+shell.cp('package.json', 'dist');
+
+ejs.renderFile('views/index.ejs', { production: true }, (err, str) => {
+  if (err) {
+    throw err;
+  } else {
+    fs.writeFile('dist/index.html', str, 'utf8', (e) => {
+      if (e) {
+        console.error(e);
+      }
+    });
+  }
+});
+
+webpack([mainConfig, rendererConfig], (err, stats) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  }
+  console.log(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false,
+  }));
+});
