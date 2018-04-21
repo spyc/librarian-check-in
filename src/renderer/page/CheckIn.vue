1e04ddeae616f08e5ff02e8be9edbfca7c99339e
Tony Yip <tony@opensource.hk>
2018-04-21 19:00:22 +0800

    Add check in UI

---
+++ src/renderer/page/CheckIn.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-form>
+        <v-container>
+            <v-layout row wrap>
+                <v-flex xs6>
+                    <v-subheader>Student ID</v-subheader>
+                </v-flex>
+                <v-flex xs6>
+                    <v-text-field
+                            autofocus
+                            name="pycid"
+                            label="Student ID"
+                            v-model="id"
+                    ></v-text-field>
+                </v-flex>
+            </v-layout>
+            <v-btn color="success">Record</v-btn>
+            <v-btn color="error" @click="clear">Clear</v-btn>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        id: '',
+      };
+    },
+    methods: {
+      clear() {
+        this.id = '';
+      },
+    },
+  };
+</script>
--- src/renderer/page/Record.vue
+++ src/renderer/page/Record.vue
@@ -1,3 +1,27 @@
 <template>
-    <v-content app></v-content>
+    <v-content app>
+        <v-container>
+            <v-card>
+                <v-tabs v-model="active" color="primary" dark slider-color="secondary">
+                    <v-tab ripple>Check In</v-tab>
+                    <v-tab-item>
+                        <check-in></check-in>
+                    </v-tab-item>
+                </v-tabs>
+            </v-card>
+        </v-container>
+    </v-content>
 </template>
+
+<script>
+  export default {
+    components: {
+      CheckIn: () => import('./CheckIn.vue'),
+    },
+    data() {
+      return {
+        active: null,
+      };
+    },
+  };
+</script>
