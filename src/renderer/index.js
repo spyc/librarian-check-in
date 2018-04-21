44f39131c5d519331a1e87f529735964ac5d5e69
Tony Yip <tony@opensource.hk>
2018-04-21 16:28:56 +0800

    Add theme color

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import colors from 'vuetify/es5/util/colors';
 import App from './App.vue';
 import createStore from './store';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#1a9eed',
+    secondary: colors.amber.accent4,
+  },
+});
 
 const store = createStore();
 
-new Vue({
+const vm = new Vue({
   store,
   render: h => h(App),
-}).$mount('#app');
+});
+vm.$mount('#app');
 
