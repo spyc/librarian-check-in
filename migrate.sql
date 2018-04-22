ff285ff11ef18055e7aebb5db8556cdb172b79f5
Tony Yip <tony@opensource.hk>
2018-04-22 17:04:43 +0800

    Add migration SQL

---
+++ migrate.sql
@@ -0,0 +1,15 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE librarian (
+    id CHARACTER(8) NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    class CHARACTER(2) NOT NULL,
+    class_no VARCHAR(2) NOT NULL
+);
+
+CREATE TABLE record(
+    id CHARACTER(8) NOT NULL,
+    check_in INTEGER NOT NULL,
+    check_out INTEGER DEFAULT NULL,
+    rank TEXT DEFAULT NULL
+);
