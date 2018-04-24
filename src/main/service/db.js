37db2eddd0b8b2c6888a34cd47fd57cc007d184d
Tony Yip <tony@opensource.hk>
2018-04-24 17:29:52 +0800

    Fix open db problem

--- src/main/service/db.js
+++ src/main/service/db.js
@@ -1,13 +1,16 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
 module.exports = function openDB() {
   return new Promise((resolve, reject) => {
-    const db = new sqlite3.Database(`${process.cwd()}/data/record.sqlite`, sqlite3.OPEN_READWRITE, (e) => {
+    const file = path.join(process.cwd(), 'data/record.sqlite');
+    const db = new sqlite3.Database(file, sqlite3.OPEN_READWRITE, (e) => {
       if (e) {
         reject(e);
       } else {
         resolve(db);
       }
     });
-  });
+  })
+    .catch(console.error);
 };
